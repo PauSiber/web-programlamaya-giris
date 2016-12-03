5eb0c6c17e3f0cfb8fc6f7bd5ba76c8ba394cc89
Gürkan Şen <Grkan en>
2016-12-03 12:26:32 +0300

    ilk duzenleme

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+  <head>
+    <title>Post IT</title>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <p>Paüsiber</p>
+    <p class="kirmizi">Gürkan Şen</p>
+    <p id="mavi" class="kirmizi">Erdem Oflaz</p>
+    <p>Koray Ateş</p>
+
+    <a href="http://google.com">Bana tıkla</a>
+    <br>
+    <img src="https://static.pexels.com/photos/29859/pexels-photo-29859.jpg" alt="Resim Adı" width="100">
+
+  </body>
+
+
+</html>
